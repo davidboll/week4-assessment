66d2c1d9494c7ca8a72ebf20ccfb12bd23ea65a7
kalle <kmthorsnes@gmail.com>
2018-02-17 11:16:19 +0100

    passed test when user visits the site

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,1 +1,7 @@
-# put step definitions in this file
+When(/^the user visits the site$/) do
+  visit '/'
+end
+
+Then(/^he should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
